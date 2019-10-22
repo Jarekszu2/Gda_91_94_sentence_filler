72ed2001286dbd911181815c101e190d5459dd48
Jarekszu2 <jarekszu2@onet.pl>
2019-10-23 01:07:17 +0200

    first commit

---
+++ src/main/java/com/j25/cloud/sentencefiller/controller/SentenceFillerController.java
@@ -0,0 +1,41 @@
+package com.j25.cloud.sentencefiller.controller;
+
+import com.j25.cloud.sentencefiller.api.Randomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SentenceFillerController {
+    // localhost:8061/fill?sentence=Siema jestem sobie taki Marcin 00###!
+
+    @Autowired
+    private Randomizer randomizer;
+
+    @GetMapping("/fill")
+    public String fill(@RequestParam(name = "sentence", required = false) String sentence) {
+        if (sentence != null) {
+            // Todo: zastąp wszystkie $$$ słowem j25
+            while (sentence.contains("!!!")) {
+
+                sentence = sentence.replaceFirst("!!!", getRandomNumber());
+            }
+            while (sentence.contains("@@@")) {
+
+                sentence = sentence.replaceFirst("@@@", getRandomWord());
+            }
+            // Todo: zastąp wszystkie ### liczbą 7
+            return sentence; /*sentence PO ZAMIANIE*/
+        }
+        return "Proszę o podanie zdania do wypełnienia";
+    }
+
+    private String getRandomWord() {
+        return randomizer.randomWord();
+    }
+
+    private String getRandomNumber() {
+        return randomizer.randomNumber();
+    }
+}
